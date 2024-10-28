f2f69ec689254fdafb19472a419fd3a0760be15f
Shagun-katariya <126535072+Shagun-katariya@users.noreply.github.com>
2024-10-28 17:24:40 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import "@/styles/globals.css";
+//Global Layout and Styling
+//Persistent State and Context Providers
+//Customizing the App's Root Structure:
+//Page-Level Props
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
